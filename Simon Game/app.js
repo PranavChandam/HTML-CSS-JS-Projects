95b4bcd11d3084780c9fa130b6f65a9ef15d7385
Pranav Chandam <pranavchandam898@gmail.com>
2025-04-10 14:12:43 +0530

    Updated Simon Game - app.js progress

---
+++ Simon Game/app.js
@@ -0,0 +1,12 @@
+let Gameseq=[]
+let userseq=[]
+
+let started=false;
+let level=0
+
+document.addEventListener('keypress',function(){
+    if(started==false){
+        console.log("Game is Started")
+        started=true
+    }
+})
