078d6c902da5bbeda55b7f904d7450ce1e9868ba
Pranav Chandam <pranavchandam898@gmail.com>
2025-04-10 15:01:44 +0530

    Update Css and Js files

--- Simon Game/app.js
+++ Simon Game/app.js
@@ -1,12 +1,37 @@
 let Gameseq=[]
 let userseq=[]
 
+let btns=['yellow','red','purple','green']
+
 let started=false;
 let level=0
+
+let h2=document.querySelector('h2')
 
 document.addEventListener('keypress',function(){
     if(started==false){
         console.log("Game is Started")
         started=true
     }
+
+
+    levelUp()
 })
+
+function btnFlash(btn){
+  btn.classList.add('Flash')
+
+  setTimeout(function(){
+    btn.classList.remove("Flash")
+  },1000)
+}
+
+function levelUp(){
+    level++
+    h2.innerText=`Level ${level}`
+    
+    let randIdx=Math.floor(Math.random()*4)
+    let randColor=btns[randIdx]
+    let randBtn=document.querySelector(`.${randColor}`)
+    btnFlash(randBtn)
+}
